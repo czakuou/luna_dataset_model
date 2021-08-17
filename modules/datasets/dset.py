52038742883834d56d8d8a7f4c763e8963095e6d
czakuou <emil.krzakuou@gmail.com>
2021-08-17 15:55:27 +0200

    added utils.py

--- modules/datasets/dset.py
+++ modules/datasets/dset.py
@@ -6,6 +6,12 @@ from functools import lru_cache
 from collections import defaultdict
 from typing import NamedTuple
 
+import SimpleITK as sitk
+import numpy as np
+
+from ..util.utils import XyzTuple, IrcTuple
+from ..util.utils import xyz2irc, irc2xyz
+
 
 DIRECTORY = '/home/czaku/Documents/Pytorch/data/'
 
@@ -64,6 +70,55 @@ def get_candidate_info_list(requireOnDisk_bool: bool = True) -> list:
     candidate_info_list.sort(reverse=True)
     return candidate_info_list
 
+
+class Ct:
+    def __init__(self, series_uid):
+        mhd_path = glob(os.path.join(DIRECTORY, f'subset*/{series_uid}.mhd'))[0]
+
+        ct_mhd = sitk.ReadImage(mhd_path)
+        ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32)
+        # CT scan voxels are expressed in HU
+        # https://en.wikipedia.org/wiki/Hounsfield_scale
+        # air is -1000, soft tissue 100-300, bone 1000
+        ct_a.clip(-1000, 1000, ct_a)
+
+        self.series_uid = series_uid
+        self.hu_a = ct_a
+
+        self.origin_xyz = XyzTuple(*ct_mhd.GetOrigin())
+        self.vxSize_xyz = XyzTuple(*ct_mhd.GetSpacing())
+        self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3, 3)
+
+    def get_rat_candidate(self, center_xyz: list[float],
+                          width_irc: list[int]) -> 'ct_chunk, center_irc':
+        center_irc = xyz2irc(
+            center_xyz,
+            self.origin_xyz,
+            self.vxSize_xyz,
+            self.direction_a,)
+
+        slice_list = []
+        for axis, center_val in enumerate(center_irc):
+            start_ndx = int(round(center_val - width_irc[axis]/2))
+            end_ndx = int(start_ndx + width_irc[axis])
+
+            rise_assert = center_val >= 0 and center_val < self.hu_a.shape[axis]
+            assert rise_assert, repr([self.series_uid, center_xyz, self.origin_xyz,
+                                      self.vxSize_xyz, center_irc, axis])
+
+            if start_ndx < 0:
+                start_ndx = 0
+                end_ndx = int(width_irc[axis])
+
+            if end_ndx > self.hu_a.shape[axis]:
+                end_ndx = self.hu_a.shape[axis]
+                start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
+
+            slice_list.append(slice(start_ndx, end_ndx))
+
+        ct_chunk = self.hu_a[tuple(slice_list)]
+
+        return ct_chunk, center_irc
 
 if __name__ == '__main__':
     print(get_candidate_info_list()[:5])
---
+++ modules/util/utils.py
@@ -0,0 +1,51 @@
+from typing import NamedTuple
+
+import numpy as np
+
+
+class XyzTuple(NamedTuple):
+    x: float
+    y: float
+    z: float
+
+
+class IrcTuple(NamedTuple):
+    index: int
+    row: int
+    col: int
+
+
+def irc2xyz(coord_irc: list[int], origin_xyz: XyzTuple,
+            vxSize_xyz: XyzTuple, direction_a: np.array[int]) -> XyzTuple:
+    """
+    Flip the coordinates of CT scans to align with XYZ
+    Scale the indices with voxel sizes
+    :param coord_irc:
+    :param origin_xyz:
+    :param vxSize_xyz:
+    :param direction_a:
+    :return: XyzTuple
+    """
+    cri_a = np.array(coord_irc)[::-1]
+    origin_a = np.array(origin_xyz)
+    vxSize_a = np.array(vxSize_xyz)
+    coords_xyz = (direction_a @ (cri_a * vxSize_a)) + origin_a
+    return XyzTuple(*coords_xyz)
+
+def xyz2irc(coord_xyz: list[float], origin_xyz: list[float],
+            vxSize_xyz: list[float], direction_a: list[float]) -> IrcTuple:
+    """
+    Convert XYZ coordinates to IRC
+    :param coord_xyz:
+    :param origin_xyz:
+    :param vxSize_xyz:
+    :param direction_a:
+    :return: IrcTuple
+    """
+    origin_a = np.array(origin_xyz)
+    vxSize_a = np.array(vxSize_xyz)
+    coord_a = np.array(coord_xyz)
+    cri_a = ((coord_a - origin_a) @ np.linalg.inv(direction_a)) / vxSize_a
+    cri_a = np.round(cri_a)
+    return IrcTuple(int(cri_a[2], int(cri_a[1]), int(cri_a[0])))
+
